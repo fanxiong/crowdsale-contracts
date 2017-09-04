76cd6f0823b007be0cf7126368f2179afd86d79e
Roderik van der Veer <roderik@settlemint.com>
2017-09-04 18:19:11 +0200

    feat: add antispam and pausing

--- contracts/EarlyTokenSale.sol
+++ contracts/EarlyTokenSale.sol
@@ -15,9 +15,13 @@ contract EarlyTokenSale is TokenController, Controlled {
     
     // 15% of tokens hard cap, at 1200 tokens per ETH
     // 225,000,000*0.15 => 33,750,000 / 1200 => 28,125 ETH => 28,125,000,000,000,000,000,000 wei
-    uint256 public maximumFunding = 28125000000000000000000;
-
-    uint256 tokensPerEther = 1200; 
+    uint256 constant public maximumFunding = 28125000000000000000000;
+    uint256 constant public tokensPerEther = 1200; 
+    uint256 constant public maxGasPrice = 50000000000;
+    
+    // antispam
+    uint256 constant public maxCallFrequency = 100;
+    mapping (address => uint256) public lastCallBlock; 
 
     // total amount raised in wei
     uint256 public totalCollected;
@@ -27,6 +31,8 @@ contract EarlyTokenSale is TokenController, Controlled {
 
     // the funds end up in this address
     address public vaultAddress;
+
+    bool public paused;
 
     /// @param _startFundingTime The UNIX time that the EarlyTokenSale will be able to start receiving funds
     /// @param _endFundingTime   The UNIX time that the EarlyTokenSale will stop being able to receive funds
@@ -47,20 +53,21 @@ contract EarlyTokenSale is TokenController, Controlled {
         endFundingTime = _endFundingTime;
         tokenContract = DataBrokerDaoToken(_tokenAddress);
         vaultAddress = _vaultAddress;
+        paused = false;
     }
 
     /// @dev The fallback function is called when ether is sent to the contract, it
     /// simply calls `doPayment()` with the address that sent the ether as the
     /// `_owner`. Payable is a required solidity modifier for functions to receive
     /// ether, without this modifier functions will throw if ether is sent to them
-    function () payable {
+    function () payable notPaused {
         doPayment(msg.sender);
     }
 
     /// @notice `proxyPayment()` allows the caller to send ether to the EarlyTokenSale and
     /// have the tokens created in an address of their choosing
     /// @param _owner The address that will hold the newly created tokens
-    function proxyPayment(address _owner) payable returns(bool success) {
+    function proxyPayment(address _owner) payable notPaused returns(bool success) {
         return doPayment(_owner);
     }
 
@@ -89,19 +96,46 @@ contract EarlyTokenSale is TokenController, Controlled {
     ///  `_owner` assuming the EarlyTokenSale is still accepting funds
     /// @param _owner The address that will hold the newly created tokens
     function doPayment(address _owner) internal returns(bool success) {
+        require(tx.gasprice <= maxGasPrice);
+
+        // Antispam
+        // do not allow contracts to game the system
+        require(!isContract(msg.sender));
+        // limit the amount of contributions to once per 100 blocks
+        require(getBlockNumber().sub(lastCallBlock[msg.sender]) >= maxCallFrequency);
+        lastCallBlock[msg.sender] = getBlockNumber();
+
         // First check that the EarlyTokenSale is allowed to receive this donation
         require(startFundingTime <= now);
         require(endFundingTime > now);
         require(tokenContract.controller() != 0);
         require(msg.value > 0);
         require(totalCollected.add(msg.value) <= maximumFunding);
+
         // Track how much the EarlyTokenSale has collected
         totalCollected = totalCollected.add(msg.value);
+
         //Send the ether to the vault
         require(vaultAddress.send(msg.value));
+
         // Creates an equal amount of tokens as ether sent. The new tokens are created in the `_owner` address
         require(tokenContract.generateTokens(_owner, tokensPerEther.mul(msg.value)));
+        
         return true;
+    }
+
+    /// @dev Internal function to determine if an address is a contract
+    /// @param _addr The address being queried
+    /// @return True if `_addr` is a contract
+    function isContract(address _addr) constant internal returns (bool) {
+        if (_addr == 0) {
+            return false;
+        }
+        uint256 size;
+        assembly {
+            size := extcodesize(_addr)
+        }
+        return (size > 0);
     }
 
     /// @notice `finalizeSale()` ends the EarlyTokenSale. It will generate the platform and team tokens
@@ -120,24 +154,48 @@ contract EarlyTokenSale is TokenController, Controlled {
     }
 
 //////////
+// Testing specific methods
+//////////
+
+    /// @notice This function is overridden by the tests.
+    function getBlockNumber() internal constant returns (uint256) {
+        return block.number;
+    }
+
+//////////
 // Safety Methods
 //////////
 
-  /// @notice This method can be used by the controller to extract mistakenly
-  ///  sent tokens to this contract.
-  /// @param _token The address of the token contract that you want to recover
-  ///  set to 0 in case you want to extract ether.
-  function claimTokens(address _token) onlyController {
-    if (_token == 0x0) {
-      controller.transfer(this.balance);
-      return;
+    /// @notice This method can be used by the controller to extract mistakenly
+    ///  sent tokens to this contract.
+    /// @param _token The address of the token contract that you want to recover
+    ///  set to 0 in case you want to extract ether.
+    function claimTokens(address _token) onlyController {
+        if (_token == 0x0) {
+            controller.transfer(this.balance);
+            return;
+        }
+
+        ERC20Token token = ERC20Token(_token);
+        uint balance = token.balanceOf(this);
+        token.transfer(controller, balance);
+        ClaimedTokens(_token, controller, balance);
     }
 
-    ERC20Token token = ERC20Token(_token);
-    uint balance = token.balanceOf(this);
-    token.transfer(controller, balance);
-    ClaimedTokens(_token, controller, balance);
-  }
+    event ClaimedTokens(address indexed _token, address indexed _controller, uint _amount);
 
-  event ClaimedTokens(address indexed _token, address indexed _controller, uint _amount);
+  /// @notice Pauses the contribution if there is any issue
+    function pauseContribution() onlyController {
+        paused = true;
+    }
+
+    /// @notice Resumes the contribution
+    function resumeContribution() onlyController {
+        paused = false;
+    }
+
+    modifier notPaused() {
+        require(!paused);
+        _;
+    }
 }
